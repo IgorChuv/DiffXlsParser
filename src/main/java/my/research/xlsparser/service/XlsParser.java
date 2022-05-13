13348646345e5428e24b3432aa019935a2b88a9f
ichuvilov <igorchuv74@gmail.com>
2022-05-13 10:44:13 +0500

    Теперь алгоритм приводит к одному регистру и удаляет лишние пробелы

--- src/main/java/my/research/xlsparser/service/XlsParser.java
+++ src/main/java/my/research/xlsparser/service/XlsParser.java
@@ -18,14 +18,45 @@ public class XlsParser {
     }
 
     public static Map<String, List<String>> createDiff(Map<String, List<String>> first, Map<String, List<String>> second) {
+
+        Map<String, List<String>> firstProcessedMap = new HashMap<>();
+        Map<String, List<String>> secondProcessedMap = new HashMap<>();
+        for(Map.Entry<String, List<String>> entry : first.entrySet()){
+           String key = entry.getKey()
+                   .toLowerCase(Locale.ROOT)
+                   .trim()
+                   .replaceAll(" +", " ");
+           List<String> value = entry.getValue().stream()
+                   .filter(Objects::nonNull)
+                   .map(x -> x.toLowerCase(Locale.ROOT))
+                   .map(String::trim)
+                   .map(x -> x.replaceAll(" +", " "))
+                   .collect(Collectors.toList());
+            firstProcessedMap.put(key,value);
+       }
+        for(Map.Entry<String, List<String>> entry : second.entrySet()){
+            String key = entry.getKey()
+                    .toLowerCase(Locale.ROOT)
+                    .trim()
+                    .replaceAll(" +", " ");
+            List<String> value = entry.getValue().stream()
+                    .filter(Objects::nonNull)
+                    .map(x -> x.toLowerCase(Locale.ROOT))
+                    .map(String::trim)
+                    .map(x -> x.replaceAll(" +", " "))
+                    .collect(Collectors.toList());
+            secondProcessedMap.put(key,value);
+        }
+
+
         Map<String, List<String>> result = new HashMap<>();
 
-        for (Map.Entry<String, List<String>> entry : first.entrySet()) {
-            if (!second.containsKey(entry.getKey())) {
+        for (Map.Entry<String, List<String>> entry : firstProcessedMap.entrySet()) {
+            if (!secondProcessedMap.containsKey(entry.getKey())) {
                 result.put(entry.getKey(), entry.getValue());
             } else {
                 List<String> firstList = entry.getValue();
-                List<String> secondList = second.get(entry.getKey());
+                List<String> secondList = secondProcessedMap.get(entry.getKey());
                 List<String> notContainsInSecondList = firstList.stream()
                         .filter(x -> !secondList.contains(x))
                         .collect(Collectors.toList());
@@ -35,6 +66,5 @@ public class XlsParser {
             }
         }
         return result;
-
     }
 }
